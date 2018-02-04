0f2b2386ef68f896aca165746489fd86dc63a6f5
lt963172 <lthomas22@att.net>
2018-02-04 16:21:02 -0600

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string MyRandom()
+        {
+            Random myrand = new Random();
+            return "The Random Number is " + myrand.Next();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
